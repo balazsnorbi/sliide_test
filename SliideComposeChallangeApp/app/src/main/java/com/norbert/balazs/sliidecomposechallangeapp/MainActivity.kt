dd40010661d9a5aa4cbd0a7f64af8f0ec26a3cf3
Norbert <norbert.balazs@ness.com>
2022-07-15 15:54:47 +0300

    Save link to the navigation

--- SliideComposeChallangeApp/app/src/main/java/com/norbert/balazs/sliidecomposechallangeapp/MainActivity.kt
+++ SliideComposeChallangeApp/app/src/main/java/com/norbert/balazs/sliidecomposechallangeapp/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
+// https://developer.android.com/jetpack/compose/navigation
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
